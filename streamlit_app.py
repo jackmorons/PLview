96c47cb5299697a167ff10f62bbf8f1f256aefed
jackmorons <giacomogiovanni.moroni@mail.polimi.it>
2026-03-16 15:43:38 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,7 +51,7 @@ def inject_custom_css():
             font-weight: 800;
             color: #f0f0f5;
             letter-spacing: -0.5px;
-            margin-top: -10px;
+            margin-top: -5px;
         }
         .nav-logo span {
             color: #d32f2f;
