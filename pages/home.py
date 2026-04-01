f122850b7a5bd1e108ff96278a0cfaaa4affb0fc
jackmorons <giacomogiovanni.moroni@mail.polimi.it>
2026-04-01 12:44:43 +0200

    Update home.py

--- pages/home.py
+++ pages/home.py
@@ -7,49 +7,6 @@ st.markdown('<div style="margin-top: 2rem;"></div>', unsafe_allow_html=True)
 st.markdown('<div class="hero-subtitle">Powerlifting is not just about lifting weights, it is about pushing yourself to the limit.</div>', unsafe_allow_html=True)
 st.markdown('<div class="hero-subtitle">Explore athletes, records, and data from the world of powerlifting, all in one place.</div>', unsafe_allow_html=True)
 
-
-#col1, col2, col3 = st.columns([1, 1, 1])
-#with col2:
-#    st.markdown("""
-#        <style>
-#        div[data-testid="stElementContainer"]:has(.athlete-btn-marker) + div[data-testid="stElementContainer"] button {
-#            background-color: rgba(211, 47, 47, 0.1);
-#            border: 1px solid #d32f2f;
-#            color: #ef5350;
-#            transition: all 0.2s ease;
-#        }
-#        div[data-testid="stElementContainer"]:has(.athlete-btn-marker) + div[data-testid="stElementContainer"] button:hover {
-#            background-color: #d32f2f;
-#            color: #ffffff;
-#            border: 1px solid #d32f2f;
-#        }
-#        </style>
-#        <div class="athlete-btn-marker"></div>
-#    """, unsafe_allow_html=True)
-#    if st.button("Browse Athletes", type="primary", use_container_width=True):
-#        st.switch_page("pages/athletes.py")
-
-#col4, col5, col6 = st.columns([1, 1, 1])
-#with col5:
-#    st.markdown("""
-#        <style>
-#        div[data-testid="stElementContainer"]:has(.record-btn-marker) + div[data-testid="stElementContainer"] button {
-#            background-color: rgba(25, 118, 210, 0.1);
-#            border: 1px solid #1976d2;
-#            color: #64b5f6;
-#            transition: all 0.2s ease;
-#        }
-#        div[data-testid="stElementContainer"]:has(.record-btn-marker) + div[data-testid="stElementContainer"] button:hover {
-#            background-color: #1976d2;
-#            color: #ffffff;
-#            border: 1px solid #1976d2;
-#        }
-#        </style>
-#        <div class="record-btn-marker"></div>
-#    """, unsafe_allow_html=True)
-#    if st.button("Browse Records", type="primary", use_container_width=True):
-#        st.switch_page("pages/record.py")
-
 st.markdown("---")
 
 # Feature Cards
@@ -74,13 +31,20 @@ st.markdown('<div style="margin-top: 2rem;"></div>', unsafe_allow_html=True)
 # Gallery Grid
 gallery = st.columns([0.6, 5, 0.2, 5, 0.4])
 with gallery[1]:
+    # first
     st.image("https://www.repelbullies.com/cdn/shop/articles/ed_coan_1024x1024.jpg?v=1604244278")
     st.markdown("Ed Coan", text_alignment="center")
+    # second
     st.image("https://i.vimeocdn.com/video/677094309-127c02ce090a4117d20683950924113a4e21a27cfeefc4c750ffa111fa1f50b4-d")
     st.markdown("Konstantīns Konstantinovs", text_alignment="center")
+    # third
+    st.image("https://www.instagram.com/p/DKvO7GwChm2/?img_index=1/media/?size=l")
+    st.markdown("Austin Perkins", text_alignment="center")
+
+
+
 with gallery[3]:
     st.image("https://i0.wp.com/www.muscleandfitness.com/wp-content/uploads/2018/06/andrey-1109.jpg?quality=86&strip=all")
     st.markdown("Andrey Malanichev", text_alignment="center")
     st.image("https://www.ironcompany.com/media/magefan_blog/2018/04/Larry-Pacifico-Squat.jpg")
     st.markdown("Larry Pacifico", text_alignment="center")
-
