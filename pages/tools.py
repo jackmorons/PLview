9380da4fb2bcc24f7604c5e0448f38d592ca1d91
jackmorons <giacomogiovanni.moroni@mail.polimi.it>
2026-04-02 12:51:04 +0200

    Update tools.py

--- pages/tools.py
+++ pages/tools.py
@@ -72,7 +72,7 @@ TOOLS_PAGES = [
     #{"key": "relative_lifts", "label": "💪 Relative Lifts"},
     {"key": "freak_finder", "label": "🤯 Strength Freaks Finder"}, # 6
     #{"key": "geo_strength", "label": "🌍 Geographical Strength"}, # 7
-    {"key": "twin_finder", "label": "🫂 Find Your PL Twin!"}, # 8
+    {"key": "twin_finder", "label": "🫂 Find Your Powerlifting Twin!"}, # 8
     {"key": "strength_index_calculator", "label": "🧮 Strength Index Calculator"}, # 9
 ]
 
