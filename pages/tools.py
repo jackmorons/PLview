ccb3897132b7bbe11055375a1c102909e7990cda
jackmorons <giacomogiovanni.moroni@mail.polimi.it>
2026-04-02 12:49:56 +0200

    Update tools.py

--- pages/tools.py
+++ pages/tools.py
@@ -4,7 +4,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 import numpy as np
 import urllib.parse
-import geopandas as gpd
+# import geopandas as gpd
 
 st.set_page_config(page_title="Tools - PLview", page_icon="⚙️", layout="wide")
 
