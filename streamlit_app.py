5bc2edcdea51c68572ea18dae8dd56d14643af78
jackmorons <giacomogiovanni.moroni@mail.polimi.it>
2026-04-09 12:44:34 +0200

    plotly

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
 
 st.set_page_config(
     page_title="PLview",
