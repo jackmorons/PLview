f35ed5e2dcedb01df17e8c6e5f7d17d0b903c62c
jackmorons <giacomogiovanni.moroni@mail.polimi.it>
2026-04-30 11:35:03 +0200

    Update style_utils.py

--- style_utils.py
+++ style_utils.py
@@ -71,7 +71,7 @@ def inject_custom_css():
             letter-spacing: -1px;
             line-height: 1.15;
             margin-bottom: 1rem;
-            background: linear-gradient(135deg, #d32f2f 30%, #d32f2f 65%, #d32f2f);
+            background: linear-gradient(135deg, #1976d2, #388e3c);
             /* background: linear-gradient(135deg, #f0f0f5 30%, #d32f2f 65%, #1976d2); */
             /* background: linear-gradient(135deg, #d32f2f 35%, #1976d2 45%, #f9a825 50%, #388e3c 55%, #e0e0e0 65%); */
             -webkit-background-clip: text;
